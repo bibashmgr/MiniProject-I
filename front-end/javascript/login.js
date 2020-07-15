9a1db3112aa8411f65dc4290be66456774667aba
bibashmgr <bibashthapamagar21@gmail.com>
2020-07-15 16:00:41 +0545

    Updating repo.

--- front-end/javascript/login.js
+++ front-end/javascript/login.js
@@ -1,8 +1,10 @@
 const hamburger = document.querySelector('.hamburger');
 const navlinks = document.querySelector('.nav-container');
 const links = document.querySelector('.navlinks li');
+const main = document.querySelector('main');
 
 
 hamburger.addEventListener('click', () => {
     navlinks.classList.toggle("open");
+    main.classList.toggle("open");
 });
