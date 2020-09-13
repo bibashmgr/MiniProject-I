4bde9eb36ef2171695ae66560b33f970ec044c7c
bibashmgr <bibashthapamagar21@gmail.com>
2020-09-13 17:39:36 +0545

    Updating repo.

--- front-end/javascript/login.js
+++ front-end/javascript/login.js
@@ -1,3 +1,5 @@
+// hamburger effect
+
 const hamburger = document.querySelector('.hamburger');
 const navlinks = document.querySelector('.nav-container');
 const links = document.querySelector('.navlinks li');
@@ -9,5 +11,31 @@ hamburger.addEventListener('click', () => {
     main.classList.toggle("open");
 });
 
-function validation(){
+// form-validation
+
+const form = document.getElementById('form');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+
+form.addEventListener('submit', (e) =>{
+    e.preventDefault();
+    checkInputs();
+});
+
+function checkInputs(){
+    const emailValue = email.value.trim();
+    const passwordValue = password.value.trim();
+
+    if(emailValue === ''){
+        setErrorFor(Email,'Email cannot be blank');
+    }
+}
+
+function setErrorFor(input,message){
+    const formControl = input.parentElement;
+    const small = formControl.querySelector('small');
+
+    small.innerText = message;
+
+    formControl.className = 'form-control error';
 }
