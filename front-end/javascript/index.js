4358ee1d8c90dc771b10b2e7372240f7fa143d2b
bibashmgr <bibashthapamagar21@gmail.com>
2020-09-14 12:26:17 +0545

    Updating repo.

--- front-end/javascript/index.js
+++ front-end/javascript/index.js
@@ -2,8 +2,10 @@
 const hamburger = document.querySelector('.hamburger');
 const navlinks = document.querySelector('.nav-links');
 const links = document.querySelector('.navlinks li');
+const main = document.querySelector('main');
 
 
 hamburger.addEventListener('click', () => {
     navlinks.classList.toggle("open");
+    main.classList.toggle("open");
 });
