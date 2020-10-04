e4a67679dcf5b729da04069253de5cb70105ed88
bibashmgr <bibashthapamagar21@gmail.com>
2020-10-05 00:15:07 +0545

    Updating repo.

--- front-end/javascript/index.js
+++ front-end/javascript/index.js
@@ -1,3 +1,5 @@
+
+// player-stats
 const slidesOne = document.querySelector('.slides-one');
 const imagesOne = document.querySelectorAll('.slides-one img');
 
@@ -34,5 +36,46 @@ slidesOne.addEventListener('transitionend',() => {
         slidesOne.style.transition = "none";
         counterOne = imagesOne.length -counterOne;
         slidesOne.style.transform = 'translateX(' + (-sizeOne * counterOne) + 'px)';
+    }
+});
+
+// gallery
+
+const slidesTwo = document.querySelector('.slides-two');
+const imagesTwo = document.querySelectorAll('.slides-two img');
+
+const prevBtnTwo = document.querySelector('#prev-btn-two');
+const nextBtnTwo = document.querySelector('#next-btn-two');
+
+let counterTwo = 1;
+const sizeTwo = imagesTwo[0].clientWidth;
+
+slidesTwo.style.transform = 'translateX(' + (-sizeTwo * counterTwo) + 'px)';
+
+nextBtnTwo.addEventListener('click',() => {
+    if(counterTwo >= imagesTwo.length -1) return;
+    slidesTwo.style.transition = "transform 0.4s ease-in-out";
+    counterTwo++;
+    slidesTwo.style.transform = 'translateX(' + (-sizeTwo * counterTwo) + 'px)';
+});
+
+prevBtnTwo.addEventListener('click',() => {
+    if(counterTwo <=0) return;
+    slidesTwo.style.transition = "transform 0.4s ease-in-out"
+    counterTwo--;
+    slidesTwo.style.transform = 'translateX(' + (-sizeTwo * counterTwo) + 'px)';
+});
+
+slidesTwo.addEventListener('transitionend',() => {
+    if(imagesTwo[counterTwo].id === 'last-clone-two'){
+        slidesTwo.style.transition = "none";
+        counterTwo = imagesTwo.length -2;
+        slidesTwo.style.transform = 'translateX(' + (-sizeTwo * counterTwo) + 'px)';
+    }
+
+    if(imagesTwo[counterTwo].id === 'first-clone-two'){
+        slidesTwo.style.transition = "none";
+        counterTwo = imagesTwo.length -counterTwo;
+        slidesTwo.style.transform = 'translateX(' + (-sizeTwo * counterTwo) + 'px)';
     }
 });
